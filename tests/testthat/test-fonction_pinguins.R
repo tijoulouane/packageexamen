e7d5c3f7083b50bcddcc54a712244783b751854f
Louane <tijoulouane@gmail.com>
2026-04-16 09:54:08 +0200

    Ajout des tests sur fonctions

--- tests/testthat/test-fonction_pinguins.R
+++ tests/testthat/test-fonction_pinguins.R
@@ -1,3 +1,46 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+# test pour la première fonction
+
+test_that("filtrer_penguins renvoie un tibble", {
+  res <- filtrer_penguins(penguins, "Adelie")
+  expect_s3_class(res, "tbl_df")
+})
+
+test_that("filtrer_penguins conserve uniquement l espece demandee", {
+  res <- filtrer_penguins(penguins, "Adelie")
+  expect_true(all(res$espece == "Adelie"))
+})
+
+test_that("filtrer_penguins renvoie 0 ligne pour une espece inconnue", {
+  res <- filtrer_penguins(penguins, "lala")
+  expect_equal(nrow(res), 0)
+})
+
+
+# Test pour la seconde fonction
+
+test_that("moyenne_poids_espece renvoie un tibble", {
+  res <- moyenne_poids_espece(penguins)
+  expect_s3_class(res, "tbl_df")
+})
+
+test_that("moyenne_poids_espece renvoie une ligne par espece", {
+  res <- moyenne_poids_espece(penguins)
+  expect_equal(nrow(res), 3)
+})
+
+test_that("moyenne_poids_espece contient les bonnes especes", {
+  res <- moyenne_poids_espece(penguins)
+  expect_setequal(res$espece, c("Adelie", "Chinstrap", "Gentoo"))
+})
+
+test_that("moyenne_poids_espece renvoie une colonne numerique", {
+  res <- moyenne_poids_espece(penguins)
+  expect_type(res$poids_moyen, "double")
+})
+
+# Test fonction avec graphique
+
+test_that("graphique_longueur_bec_max renvoie un objet ggplot", {
+  res <- graphique_longueur_bec_max(penguins)
+  expect_s3_class(res, "ggplot")
 })
