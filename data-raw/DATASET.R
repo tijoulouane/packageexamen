9c955c6f4b753e65803aaa67820202ec596fe8ca
Louane <tijoulouane@gmail.com>
2026-04-16 10:15:38 +0200

    Information du DATASET

--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -1,9 +1,12 @@
+## code to prepare `penguins` dataset goes here
+
 #install.packages("palmerpenguins")
 library(palmerpenguins)
 
 data <- penguins
-
 data <- na.omit(data)
+
+# Renommer les variables
 
 data <- data |>
   dplyr::rename(
@@ -14,15 +17,20 @@ data <- data |>
     longueur_nageoire = flipper_length_mm,
     poids = body_mass_g,
     sexe = sex,
-    annee = year)
+    annee = year
+  )
+
+# Bon type aux bonnes variables
 
 data$espece <- as.factor(data$espece)
-data$ile  <- as.factor(data$ile)
-data$sexe     <- as.factor(data$sexe)
+data$ile <- as.factor(data$ile)
+data$sexe <- as.factor(data$sexe)
 
 data$longueur_bec <- as.numeric(data$longueur_bec)
-data$profondeur_bec  <- as.numeric(data$profondeur_bec)
+data$profondeur_bec <- as.numeric(data$profondeur_bec)
 
-write.csv(data, "penguins.csv", row.names = FALSE)
-usethis::use_data(penguins, overwrite = TRUE)
+# Jeu de donnees utilise pour l'exercice
+
+usethis::use_data(data, overwrite = TRUE)
+# write.csv(data, "penguins.csv", row.names = FALSE)
 
