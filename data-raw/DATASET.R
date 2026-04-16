5cbe5401155825a0f3adb82949b8566e6a236b69
Louane <tijoulouane@gmail.com>
2026-04-16 10:10:19 +0200

    Information dans le fichier DATASET

--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -1,3 +1,26 @@
-## code to prepare `DATASET` dataset goes here
+install.packages("palmerpenguins")
+library(palmerpenguins)
 
-usethis::use_data(DATASET, overwrite = TRUE)
+data <- penguins
+
+data <- na.omit(data)
+
+data <- data |>
+  dplyr::rename(
+    espece = species,
+    ile = island,
+    longueur_bec = bill_length_mm,
+    profondeur_bec = bill_depth_mm,
+    longueur_nageoire = flipper_length_mm,
+    poids = body_mass_g,
+    sexe = sex,
+    annee = year)
+
+data$espece <- as.factor(data$espece)
+data$ile  <- as.factor(data$ile)
+data$sexe     <- as.factor(data$sexe)
+
+data$longueur_bec <- as.numeric(data$longueur_bec)
+data$profondeur_bec  <- as.numeric(data$profondeur_bec)
+
+write.csv(data, "penguins.csv", row.names = FALSE)
