9dc0e75d5ab52d1e598c69ea21af263885d25261
Louane <tijoulouane@gmail.com>
2026-04-16 09:38:01 +0200

    ducmentation jeu de données

---
+++ R/data.R
@@ -0,0 +1,24 @@
+#' @title Jeu de donnees pinguins
+#'
+#' @description Ce jeu de donnees contient des informations sur des manchots
+#' observes sur plusieurs iles entre 2007 et 2009. Chaque ligne correspond
+#' a un individu.
+#'
+#' @format Un data frame avec 333 lignes et 8 variables :
+#' \describe{
+#'   \item{espece}{Espece du manchot. Variable qualitative.}
+#'   \item{ile}{Ile d'observation. Variable qualitative.}
+#'   \item{longueur_bec}{Longueur du bec en millimetres. Variable quantitative.}
+#'   \item{profondeur_bec}{Profondeur du bec en millimetres. Variable quantitative.}
+#'   \item{longueur_nageoire}{Longueur de la nageoire en millimetres. Variable quantitative.}
+#'   \item{poids}{Poids du manchot en grammes. Variable quantitative.}
+#'   \item{sexe}{Sexe du manchot. Variable qualitative.}
+#'   \item{annee}{Annee d'observation. Variable quantitative discrete.}
+#' }
+#'
+#' @source Palmer Penguins, jeu de donnees prepare a partir du package
+#' palmerpenguins.
+#'
+#' @docType data
+#' @name pinguins
+NULL
